c3e8b0583f7c3e5e9067f2353963966123f4024c
ericleuty <ericleuty@gmail.com>
2022-02-20 15:44:35 -0500

    Cleaned up code

--- EricEM.py
+++ EricEM.py
@@ -8,6 +8,7 @@ import csv
 import scipy.fft as fft
 import scipy.stats as stats
 import scipy as sp
+import math as m
 
 
 # %%
@@ -96,6 +97,13 @@ class Interaction():
         d = np.concatenate((d_c, d_v))
         return d
 
+# %% Accessory functions
+def get_bins(num):
+    bins_hist = (np.arange(num) - num // 2) * detector_width
+    num = num - 1
+    bins_plot = (np.arange(num) - num // 2) * detector_width
+    return bins_hist, bins_plot
+
 
 # %%
 core = CoreElectron(scale=core_p_std)
@@ -107,25 +115,20 @@ pz_core = core.get_pz(num)
 d_val = i.get_z(pz_val)
 d_core = i.get_z(pz_core)
 
-num_bins = 52
-bins = (np.arange(num_bins) - num_bins // 2) * detector_width
+bins_hist_w, bins_plot_w = get_bins(52)
 
-
-hist_val = np.histogram(d_val, bins=bins)[0]
-hist_core = np.histogram(d_core, bins=bins)[0]
+hist_val = np.histogram(d_val, bins=bins_hist_w)[0]
+hist_core = np.histogram(d_core, bins=bins_hist_w)[0]
 hist_val = hist_val / np.sum(hist_val)
 hist_core = hist_core / np.sum(hist_core)
 
-fft_val = fft.fft(hist_val)
-fft_core = fft.fft(hist_core)
 
 
 # %%
-num_bins = 51
-bins = (np.arange(num_bins) - num_bins // 2) * detector_width
+
 
 fig1, ax1 = plt.subplots()
-ax1.bar(bins, hist_core, width=detector_width)
+ax1.bar(bins_plot_w, hist_core, width=detector_width)
 ax1.set_xlabel("z [m]")
 ax1.set_ylabel("Population Density")
 ax1.set_title("Core Electron Photon Deflection")
@@ -133,7 +136,7 @@ fig1.tight_layout()
 fig1.show()
 
 fig2, ax2 = plt.subplots()
-ax2.bar(bins, hist_val, width=detector_width)
+ax2.bar(bins_plot_w, hist_val, width=detector_width)
 ax2.set_xlabel("z [m]")
 ax2.set_ylabel("Population Density")
 ax2.set_title("Valence Electron Photon Deflection")
@@ -141,10 +144,10 @@ fig2.tight_layout()
 fig2.show()
 
 fig3, ax3 = plt.subplots(2, 1)
-ax3[0].bar(bins, hist_core, width=detector_width)
+ax3[0].bar(bins_plot_w, hist_core, width=detector_width)
 ax3[0].set_ylabel("Population Density")
 ax3[0].set_title("Core Electron Photon Deflection")
-ax3[1].bar(bins, hist_val, width=detector_width)
+ax3[1].bar(bins_plot_w, hist_val, width=detector_width)
 ax3[1].set_ylabel("Population Density")
 ax3[1].set_xlabel("z [m]")
 ax3[1].set_title("Valence Electron Photon Deflection")
@@ -160,17 +163,15 @@ i = Interaction(core, val)
 num_detected = 10**6
 d = i.predict_deflection(num_detected, 1.5)
 
-num_bins = 52
-bins = (np.arange(num_bins) - num_bins // 2) * detector_width
-hist_d = np.histogram(d, bins=bins)[0]
+bins_hist, bins_plot = get_bins(52)
+hist_d = np.histogram(d, bins=bins_hist)[0]
 hist_d = hist_d / np.sum(hist_d)
 
 # %%
-num_bins = 51
-bins = (np.arange(num_bins) - num_bins // 2) * detector_width
+
 
 fig4, ax4 = plt.subplots()
-ax4.bar(bins, hist_d, width=detector_width)
+ax4.bar(bins_plot, hist_d, width=detector_width)
 ax4.set_xlabel("z [m]")
 ax4.set_ylabel("Population Density")
 ax4.set_title("Deflection of photon with respect to z for Cu")
@@ -195,11 +196,10 @@ norm_counts = norm_counts / np.sum(norm_counts)
 
 
 # %%
-num_bins = 51
-pos = (np.arange(num_bins) - num_bins // 2) * detector_width
+
 
 fig5, ax5 = plt.subplots()
-ax5.bar(pos, coincident_counts, width=detector_width)
+ax5.bar(bins_plot, coincident_counts, width=detector_width)
 ax5.set_xlabel("z [m]")
 ax5.set_ylabel("Population Density")
 ax5.set_title("Observed deflection of photon with respect to z for Cu")
@@ -207,7 +207,7 @@ fig5.tight_layout()
 fig5.show()
 
 fig6, ax6 = plt.subplots()
-ax6.bar(pos, norm_counts, width=detector_width)
+ax6.bar(bins_plot, norm_counts, width=detector_width)
 ax6.set_xlabel("z [m]")
 ax6.set_ylabel("Population Density")
 ax6.set_title("Norm 1: {:.4f}".format(sp.stats.skew(norm_counts)))
@@ -220,13 +220,13 @@ fig6.show()
 def line(x, a3, a2, a1, a0):
     return a3*x**3 + a2*x**2 + a1*x + a0
 
-a = sp.optimize.curve_fit(line, pos, counts)[0]
+a = sp.optimize.curve_fit(line, bins_plot, counts)[0]
 a3 = a[0]/a[3]
 a2 = a[1]/a[3]
 a1 = a[2]/a[3]
 a0 = a[3]/a[3]
 
-line_fix = line(pos, a3, a2, a1, a0)
+line_fix = line(bins_plot, a3, a2, a1, a0)
 
 counts_2 = counts / line_fix
 co_counts_2 = coincident_counts / line_fix
@@ -234,15 +234,50 @@ norm_counts_2 = co_counts_2 / co_counts_2.sum()
 
 
 fig, ax = plt.subplots()
-ax.bar(pos, norm_counts_2, width=detector_width)
+ax.bar(bins_plot, norm_counts_2, width=detector_width)
 ax.set_xlabel("z [m]")
 ax.set_ylabel("Population Density")
 ax.set_title("Norm 2: {:.4f}".format(sp.stats.skew(norm_counts_2)))
 fig.tight_layout()
 fig.show()
 
+# %% Correct skew
+
+skew = sp.stats.skew(norm_counts)
+shift = 2
+shift_arr = np.zeros(2)
+norm_counts_3 = norm_counts[:-2*shift]
+skew_shift = sp.stats.skew(norm_counts_3)
+
+bins_hist_s, bins_plot_s = get_bins(52 - 2 * shift)
+fig, ax = plt.subplots()
+ax.bar(bins_plot_s, norm_counts_3, width=detector_width, label="Real Data")
+fig.show()
+
 # %%
-fft_real = fft.fft(norm_counts)
+core = CoreElectron(scale=core_p_std)
+val = ValenceElectron(fermi_p)
+i = Interaction(m_e, detector_length)
+num = 10**6
+pz_val = val.get_pz(num)
+pz_core = core.get_pz(num)
+d_val = i.get_z(pz_val)
+d_core = i.get_z(pz_core)
+
+bins_hist_s, bins_plot_s = get_bins(52 - 2 * shift)
+
+hist_val = np.histogram(d_val, bins=bins_hist_s)[0]
+hist_core = np.histogram(d_core, bins=bins_hist_s)[0]
+hist_val = hist_val / np.sum(hist_val)
+hist_core = hist_core / np.sum(hist_core)
+
+fft_val = fft.fft(hist_val)
+fft_core = fft.fft(hist_core)
+
+
+
+# %%
+fft_real = fft.fft(norm_counts_3)
 fig7, ax7 = plt.subplots()
 ax7.plot(fft_real, label='Real')
 ax7.plot(fft_core, label='Core')
@@ -256,54 +291,83 @@ fig7.show()
 
 
 # %%
-
+fft_real = fft.fft(norm_counts_3)
 b = np.mean(fft_real - fft_core) / np.mean(fft_val - fft_core)
 a = 1-b
+print("a guess: {:.2f}".format(a))
 
-z = a.real * hist_core + b.real * hist_val
-num_bins = 51
-pos = (np.arange(num_bins) - num_bins // 2) * detector_width
+z_fft = a.real * hist_core + b.real * hist_val
+
 fig, ax = plt.subplots()
-ax.bar(pos, norm_counts, width=detector_width, label='Real Data')
-ax.bar(pos, z, width=detector_width, label='FFT Prediction', fill=False)
+ax.bar(bins_plot_s, norm_counts_3, width=detector_width, label='Real Data')
+ax.bar(bins_plot_s, z_fft, width=detector_width, label='FFT Prediction', fill=False)
+ax.set_title("Valence ratio: {:.3f}".format(a.real))
 ax.legend()
-#fig.show()
+fig.show()
 
 # %%
-a = np.linspace(1.2, 1.6, num=100)
+b = np.mean(norm_counts_3 - hist_core) / np.mean(hist_val - hist_core)
+a = 1-b
+z = b * hist_core + a * hist_val
+print("a guess: {:.2f}".format(a))
+
+fig, ax = plt.subplots()
+ax.bar(bins_plot_s, norm_counts_3, width=detector_width, label='Real Data')
+ax.bar(bins_plot_s, z, width=detector_width, label='Linear Prediction', fill=False)
+ax.set_title("Valence ratio: {:.3f}".format(a.real))
+ax.legend()
+fig.show()
+
+# %%
+a = np.linspace(9, 10, num=100)
 num_detected = int(total_counts)
 
 core = CoreElectron(scale=core_p_std)
 val = ValenceElectron(fermi_p)
 i = Interaction(core, val)
-pred = np.zeros((a.shape[0], norm_counts.shape[0]))
+pred = np.zeros((a.shape[0], norm_counts_3.shape[0]))
 diff = np.zeros(a.shape)
 
-num_bins = 52
-bins = (np.arange(num_bins) - num_bins // 2) * detector_width
 for idx in range(len(a)):
     d = i.predict_deflection(num_detected, a[idx])
-    hist_d = np.histogram(d, bins=bins)[0]
+    hist_d = np.histogram(d, bins=bins_hist_s)[0]
     pred[idx] = hist_d / np.sum(hist_d)
-    temp_diff = pred[idx] - norm_counts
+    temp_diff = pred[idx] - norm_counts_3
     diff[idx] = np.sqrt(temp_diff.dot(temp_diff))
-
-
 
 min = np.argsort(diff, axis=0)
 
 
 # %%
-print(a[min[:5]])
-print(diff[min[:5]])
+
+num = pred.shape[0]
+
+dim = int(m.ceil(np.sqrt(pred.shape[0])))
+
+
+fig, ax = plt.subplots()
+ax.plot(a, diff)
+fig.show()
+
+
+fig, ax = plt.subplots(m.ceil(dim), m.ceil(dim))
+fig.set_size_inches(50,50)
+for i in range(num):
+
+    ax[i // dim][i % dim].bar(bins_plot_s, norm_counts_3, width=detector_width, label="Real Data")
+    ax[i // dim][i % dim].bar(bins_plot_s, pred[i], width=detector_width, label="Prediction {:.2f}".format(a[i]), fill=False)
+    ax[i // dim][i % dim].set_title("Prediction {:.2f}".format(a[i]))
+
+
+fig.show()
 
 
 # %%
 
 fig9, ax9 = plt.subplots()
-ax9.bar(pos, norm_counts, width=detector_width, label="Real Data")
-ax9.bar(pos, pred[0], width=detector_width, label="Best Prediction", fill=False)
-ax9.bar(pos, z, width=detector_width, label="Best FFT Prediction", fill=False)
+ax9.bar(bins_plot_s, norm_counts_3, width=detector_width, label="Real Data")
+ax9.bar(bins_plot_s, pred[min[0]], width=detector_width, label="Best Prediction", fill=False, edgecolor='red')
+ax9.bar(bins_plot_s, z_fft, width=detector_width, label="Best FFT Prediction", fill=False, edgecolor='green')
 ax9.legend()
 ax9.set_xlabel("z [m]")
 ax9.set_ylabel("Population Density")
@@ -312,22 +376,18 @@ fig9.tight_layout()
 fig9.show()
 
 # %%
-num_bins = 52
-bins = (np.arange(num_bins) - num_bins // 2) * detector_width
-print(a[min[0]])
-num_bins = 51
-pos = (np.arange(num_bins) - num_bins // 2) * detector_width
+
 core = CoreElectron(scale=core_p_std)
 val = ValenceElectron(fermi_p)
 i = Interaction(core, val)
-pred = i.predict_deflection(num_detected, a=a[min[0]])
-hist_pred = np.histogram(pred, bins=bins)[0]
+pred2 = i.predict_deflection(num_detected, a=a[min[0]])
+hist_pred = np.histogram(pred2, bins=bins_hist)[0]
 hist_pred = hist_pred / np.sum(hist_pred)
 
 
 fig, ax = plt.subplots()
-ax.bar(pos, norm_counts, width=detector_width, label="Real Data")
-ax.bar(pos, hist_pred, width=detector_width, label="Best Prediction", fill=False)
+ax.bar(bins_plot, norm_counts, width=detector_width, label="Real Data")
+ax.bar(bins_plot, hist_pred, width=detector_width, label="Best Prediction", fill=False)
 ax.legend()
 
 
@@ -339,6 +399,7 @@ fig.show()
 
 
 # %%
+
 
 
 
